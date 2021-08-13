d3500149549f6e681effdd9ac147a21eedd2c2d2
Sarah Shaffer <sarahsavell2@gmail.com>
2021-08-13 17:27:45 -0500

    yes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,17 +4,11 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      
-    </div>
-=======
     <Router>
       <Switch>
         <Route path="/" component={SidebarWithHeader} />
       </Switch>
     </Router>
->>>>>>> 8d914d0b4dcd61918c4ccadef948294299f35ab8
   );
 }
 
