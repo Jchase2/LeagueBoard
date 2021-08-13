c3dd9d3a0e61058e09f129dde9694df1e970cf14
Lewis <lewrodriguez18@saintpeters.edu>
2021-08-13 11:40:21 -0400

    Chakra provider added

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
