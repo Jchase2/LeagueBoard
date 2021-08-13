f82f5809c8917217cd96fcd7e75f58422225b78b
jchase2 <mr.jdchase@gmail.com>
2021-08-13 11:29:38 -0400

    Created utils folder with examples in index.ts

---
+++ client/src/utils/index.ts
@@ -0,0 +1,11 @@
+// This is a utils folder. Feel free to make files to export functions into and
+// import them into your components. This will help simplify components and allow
+// us to unit test these functions without mounting components.
+
+// Example:
+
+//utils/index.js:
+
+// export { default as DateUtils} from './DateUtils';
+// export { default as StringUtils} from './StringUtils';
+// export { default as NumberUtils} from './NumberUtils';
