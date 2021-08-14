9a8261d422c6d0075f24857e0622b767dbcc977e
jchase2 <mr.jdchase@gmail.com>
2021-08-14 13:38:43 -0400

    Restructuring to follow a domain approach for react components.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import SidebarWithHeader from "./components/Heading/Heading";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Nothing from './components/nothing/nothing';
 
 function App() {
   return (
     <Router>
+      <Nothing />
       <Switch>
         <Route path="/" component={SidebarWithHeader} />
       </Switch>
