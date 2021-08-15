ecb383d528c600b707adea90d1037de84b43412e
Sarah Shaffer <sarahsavell2@gmail.com>
2021-08-14 20:00:22 -0500

    tis

--- client/src/redux/actions/Scrimmage.ts
+++ client/src/redux/actions/Scrimmage.ts
@@ -1,26 +1,6 @@
 import { EReduxActionTypes } from '../actions/ActionTypes';
+import { IScrimmage } from '../../interfaces/Scrimmage';
 
-interface IScrimmage {
-  date: Date,
-  time: TimeRanges,
-  teams: ITeams[],
-}
-interface ITeams {
-  team1: ITeam1,
-  team2: ITeam2
-}
-interface ITeam1 {
-  name: string,
-  players: IPlayer[]
-}
-interface ITeam2 {
-  name: string,
-  players: IPlayer[]
-}
-interface IPlayer {
-  name: string,
-  id: string
-}
 // Action Creators - functions that return an action
 
 export const getAllScrimmages = () => async (dispatch: (arg: { type: string; payload: { scrimmages: IScrimmage[]; }; }) => void) => {
