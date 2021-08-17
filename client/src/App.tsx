dcaf4fef51dd3328c649043ffd9651e65aee4e74
Sarah Shaffer <sarahsavell2@gmail.com>
2021-08-17 15:54:01 -0500

    git

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,6 +10,7 @@ import VerificationComponent from "./domain/Register/VerificationComponent";
 import Topics from './domain/Forums/Topics';
 import CreateTopic from './domain/Forums/CreateTopic';
 import ThreadPage from './domain/Forums/ThreadPage';
+import { Scrimmage } from "./domain/Dashboard/Scrimmage/Scrimmage";
 
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
           <Route exact path="/topics" component={Topics}></Route>
           <Route exact path="/topics/create" component={CreateTopic}></Route>
           <Route exact path="/topics/:id" component={ThreadPage}></Route>
+          <Route exact path="/scrim" component={Scrimmage}></Route>
 
         </Switch>
       </Router>
