a1f99fa665d060858b5f62378228cd9950c59384
Sarah Shaffer <sarahsavell2@gmail.com>
2021-08-17 08:47:33 -0500

    git

--- client/src/domain/Register/VerificationComponent.tsx
+++ client/src/domain/Register/VerificationComponent.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useLocation, useHistory } from "react-router";
-import { getVerifyInfo, signUp } from "../../api/api";
+import { getVerifyInfo, signUp } from "../../api/backendApi";
 import { ErrorShow } from "../../components/Error/ErrorShow";
 
 import {
