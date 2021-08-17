4ec730ee845c7b9f891729dc152505d677ec8854
jchase2 <mr.jdchase@gmail.com>
2021-08-16 21:01:12 -0400

    Added some backend code for forums. Will leave it up to Rafel as to weather we actually use this code or not.

--- server/src/Controllers/apiController.ts
+++ server/src/Controllers/apiController.ts
@@ -1,12 +1,37 @@
 require('dotenv').config();
 import { Response, Request } from 'express';
-const { Region } = require('../Models/region.model');
+const { Region, Topic } = require('../Models/region.model');
 
 
 export const getRegions = async (req: Request, res: Response, next: Function) => {
   try {
     const regions = await Region.findAll({});
     res.json(regions);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getForumTopics = async (req: Request, res: Response, next: Function) => {
+  try {
+    const topics = await Topic.findAll({});
+    res.json(topics);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const postForumTopic = async (req: Request, res: Response, next: Function) => {
+  try {
+    const topics = await Topic.create({
+      title: req.body.title,
+      text: req.body.text,
+      userid: req.body.userId,
+      parentid: req.body?.parentid,
+      closed: false,
+    })
+    res.status(201);
+    res.json(topics);
   } catch (err) {
     next(err);
   }
