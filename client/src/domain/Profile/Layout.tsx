faf66df99a011163bfe9401eeb70d88f3b19a3d5
Sarah Shaffer <sarahsavell2@gmail.com>
2021-08-18 19:49:50 -0500

    git

--- client/src/domain/Profile/Layout.tsx
+++ client/src/domain/Profile/Layout.tsx
@@ -1,27 +1,40 @@
 import { Flex } from "@chakra-ui/react"
 import { useEffect, useState } from "react";
-// import useGetUsers from "../../utils/Getuser"
+import UserDonutGraph from "./UserDonutGraph";
+import jwt_decode from 'jwt-decode';
+import { getUserMatches } from '../../api/profileAPI';
 
 interface Props {}
 
 const Layout: React.FC<Props> = (props: Props) => {
   // use custom react hook, import use User hook, deconstruct outside
   // dont decode here, decode tokens in the backend, gets the user ID and get information attach to the header of the request
-  // let decoded: any;
-  // const user: string | null = localStorage.getItem("accessToken");
-  // if (user) {
-  //   decoded = jwt_decode(user);
-  // }
-  // if (decoded) {
-  //   getUserMatches(decoded?.user?.puuid).then((res) => setuserMatches(res));
-  // }
-  // console.log(userMatches);
-  // console.log(decoded)
-  // const user = useGetUsers();
-  // console.log(user)
+  const [current, setCurrent] = useState<any>()
+  const [userMatches, setUserMatches] = useState()
+  let decoded: any;
+  const user:any = localStorage.getItem("accessToken");
+  useEffect(() => {
+    if (user) {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      decoded = jwt_decode(user);
+      setCurrent(decoded);
+
+      console.log(current);
+      
+    }
+    if (decoded) {
+      getUserMatches(current?.puuid).then((res) => setUserMatches(res));
+      console.log(userMatches)
+    }
+  }, [])
+  
+
+  //const user = useGetUsers();
+
   return (
     <Flex justifyContent="center" alignContent="center">
       Hello world
+      
     </Flex>
   );
 }
---
+++ client/src/domain/Profile/UserDonutGraph.tsx
@@ -0,0 +1,174 @@
+import React, { useEffect, useState} from 'react'
+import { CanvasJSChart } from 'canvasjs-react-charts';
+import { getRecentMatches } from '../../../../api/backendApi';
+import { useSelector } from 'react-redux';
+import jwt_decode from 'jwt-decode';
+import { getUserMatches } from '../../../../api/profileAPI';
+import { allMatches } from './matchData';
+import { Container, Radio, RadioGroup, Stack } from "@chakra-ui/react"
+
+const UserDonutGraph = ({team1, team2}:any) => {
+  const [userHistory, setUserHistory] = useState<any[]>([])
+  const [userMatches, setuserMatches] = useState<any[]>([])
+  const [userDeathHistory, setUserDeathHistory] = useState<any>({});
+  const [userKillHistory, setUserKillHistory] = useState<any>({});
+  const [userAssistHistory, setUserAssistHistory] = useState<any>({});
+  const [userValue, setUserValue] = useState<any>({}) 
+  const [graph, setGraph] = useState<any>('');
+  
+  console.log(userDeathHistory, userKillHistory, userAssistHistory);
+
+    useEffect(() => {
+      console.log("2nd use effect")
+      setUserValue(userKillHistory)
+      setGraph("kills")
+    }, [userKillHistory])
+
+
+    useEffect(() => {
+      let currentUser = '';
+      let decoded: any
+      const user: string | null = localStorage.getItem("accessToken");
+      if (user) {
+        decoded = jwt_decode(user);
+      }
+      const match = async () => {
+         currentUser = decoded?.user.summoner_name;
+        console.log(currentUser)
+        let array:any = [];
+        getUserMatches(decoded?.user?.puuid).then(async (res) => {
+          
+          setUserHistory(await array)
+        } );
+         
+        //let current = 'demon6kitty2';
+        
+          let userKills = {avg: 0, high: 0, low: 0};
+          let userDeaths = {avg: 0, high: 0, low: 0};
+          let userAssists = {avg: 0, high: 0, low: 0};
+          let j:number = 0;
+          let resultArr:any[] = [];
+          let kills:any[] = [];
+          let deaths:any[] = [];
+          let assists:any[] = [];
+          for (let i:number = 0; i < userMatches.length; i++) {
+            console.log(userMatches)
+            let matchInfo = userMatches[i][j];
+            let participants = matchInfo.info['participants'];
+            participants.forEach(element => {
+              if (element['summonerName'] === currentUser) resultArr.push(element); 
+            });  
+            j++;
+          }
+        resultArr.forEach(match => {
+          kills.push(match['kills'])
+        })
+        if (kills.length !== 0) {
+          userKills.high = Math.max(...kills);
+          userKills.low = Math.min(...kills);
+          userKills.avg = Math.round(kills.reduce((a, b) => (a + b)) / kills.length); 
+          
+          setUserKillHistory(userKills);
+        }
+        
+  
+        resultArr.forEach(match => {
+          deaths.push(match['deaths'])
+        })
+        if (deaths.length !== 0) {
+          userDeaths.high = Math.max(...deaths);
+          userDeaths.low = Math.min(...deaths);
+          console.log(...deaths)
+          userDeaths.avg = deaths.reduce((a, b) => (a + b)) / deaths.length;
+          setUserDeathHistory(userDeaths);
+        }
+  
+        resultArr.forEach(match => {
+          assists.push(match['assists'])
+        })
+        if (assists.length !== 0) {
+          userAssists.high = Math.max(...assists);
+          userAssists.low = Math.min(...assists);
+          userAssists.avg = assists.reduce((a, b) => (a + b)) / assists.length;
+          setUserAssistHistory(userAssists);
+        }
+        
+        }
+        
+      
+      match();
+      
+    }, [])
+    
+    
+   
+    
+    const setValue = (value: string) => {
+      console.log("set value", value)
+      if (value === 'kills') {
+        setUserValue(userKillHistory)
+        setGraph(value)
+      }
+      if (value === 'deaths') {
+        setUserValue(userDeathHistory)
+        setGraph(value)
+      }
+      if (value === 'assists') {
+        setUserValue(userAssistHistory)
+        setGraph(value)
+      }
+      
+    };
+
+
+  
+  return (
+    <div>
+      <Container>
+        <RadioGroup defaultValue={'kills'} onChange={(value) => setValue(value)}>
+          <Stack direction="row">
+            <Radio value="kills">kills</Radio>
+            <Radio value="deaths">deaths</Radio>
+            <Radio value="assists">assists</Radio>
+          </Stack>
+        </RadioGroup>
+        {console.log(userValue)}
+         <CanvasJSChart
+          options={{
+            colorSet:  "customColorSet1",
+            theme: "light2",
+            title: {
+              text: ``,
+              fontColor: "#b5b5c6",
+            },
+            subtitles: [{
+              text: graph,
+              verticalAlign: "center",
+              fontSize: 24,
+              dockInsidePlotArea: true
+            }],
+            
+            backgroundColor: "#ffffff",
+            data: [{
+              type: "doughnut",
+              //indexLabel: "{name}: {y}",
+              yValueFormatString: "##",
+              dataPoints: [
+                { name: "avg", y: userValue.avg },
+                { name: "low", y: userValue.low },
+                { name: "high", y: userValue.high }
+              ]
+            }]
+          } } 
+
+        />
+
+        </Container>
+      
+    </div>
+  )
+}
+
+export default UserDonutGraph
+
+
