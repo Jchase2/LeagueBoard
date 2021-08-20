f73ce94ae2aa7138c251d1852540ff063ffdc157
RafelCorsten <rafelcorsten@gmail.com>
2021-08-20 17:28:11 -0300

    Changed mapTopics component to recieve topics in REACT.FC. Changed Topics, deleted type on topics variable

--- client/src/domain/Forums/MapTopics.tsx
+++ client/src/domain/Forums/MapTopics.tsx
@@ -1,14 +1,6 @@
 import ThreadCard from "./ThreadCard";
-import { useAppDispatch, useAppSelector } from "../../redux/hooks";
-import { useEffect } from "react";
-import { fetchForumTopics } from "../../redux/slices/topicsSlice";
 
-const MapTopics: React.FC = ({topics}: any) => {
-  const dispatch = useAppDispatch()
-  const threadArray = useAppSelector((state) => state.topicsReducer.topics);
-  useEffect(() => {
-    dispatch(fetchForumTopics());
-  }, [dispatch]);  
+const MapTopics: React.FC<{topics:any}> = ({topics}) => {
   return (
     <>
       {
