a9c3f8e3a7d566d11f1ca2508053b02754ded23f
jchase2 <mr.jdchase@gmail.com>
2021-08-21 12:26:26 -0400

    Checking if user owns a thread before displaying delete thread option.

--- client/src/domain/Forums/ReplyOrDelete.tsx
+++ client/src/domain/Forums/ReplyOrDelete.tsx
@@ -1,41 +1,54 @@
 import { Button } from "@chakra-ui/react";
 import { Props } from "framer-motion/types/types";
-import { useAppDispatch } from "../../redux/hooks";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { deleteForumTopic, fetchComments } from "../../redux/slices";
 import { useHistory } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getTopicOwner } from "../../api/api";
 
 const ReplyOrDelete: React.FC<Props> = ({ isReply, setIsReply, thread }) => {
   const dispatch = useAppDispatch();
   let history = useHistory();
+  const user = useAppSelector((state) => state.userReducer.userState);
 
+  const [threadCreator, setThreadCreator] = useState<String>("");
+
+  useEffect(() => {
+    getTopicOwner(thread.id).then((owner) => {
+      setThreadCreator(owner?.summoner_name);
+    });
+  }, [thread.id]);
 
   const handleDelete = () => {
-    console.log("handleDelete")
     dispatch(deleteForumTopic(thread.id)).then(() => {
-      console.log("dispatch then")
-      dispatch(fetchComments(thread.parentid))
+      dispatch(fetchComments(thread.parentid));
     });
-    if(!thread.parentid){
-      history.push('/topics')
+    if (!thread.parentid) {
+      history.push("/topics");
     }
   };
 
   const handleSubmit = () => {
-    setIsReply(true)
-    dispatch(fetchComments(thread.id))
-  }
+    setIsReply(true);
+    dispatch(fetchComments(thread.id));
+  };
 
   return !isReply ? (
     <>
-      {!thread.closed &&
-      <Button onClick={() => handleSubmit()} m={1}>
-        Reply
-      </Button>}
-      <Button m={1} onClick={handleDelete}>
-        Delete
-      </Button>
+      {!thread.closed && (
+        <Button onClick={() => handleSubmit()} m={1}>
+          Reply
+        </Button>
+      )}
+      {threadCreator === user.summoner_name && (
+        <Button m={1} onClick={handleDelete}>
+          Delete
+        </Button>
+      )}
     </>
-  ) : <></>
+  ) : (
+    <></>
+  );
 };
 
 export default ReplyOrDelete;
