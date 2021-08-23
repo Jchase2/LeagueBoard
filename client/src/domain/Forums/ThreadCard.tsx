eb4b1d0b9bc69b0e243e0f6eca3a7d5913cc9485
jchase2 <mr.jdchase@gmail.com>
2021-08-23 19:56:52 -0400

    Fixed some styling for ThreadPage

--- client/src/domain/Forums/ThreadCard.tsx
+++ client/src/domain/Forums/ThreadCard.tsx
@@ -6,27 +6,16 @@ import { ByComp, UpOrDownVote } from "./";
 
 const Forums: React.FC<Props> = ({ thread }) => {
   return (
-    // <Link to={`/topic/${thread.id}`}>
-    //   {thread.title}
-    //   {thread.id && <UpOrDownVote thread={thread} />}
-    //   <Text isTruncated>{thread.text}</Text>
-    //   {thread.id && <ByComp thread={thread} />}
-    // </Link>
     <Flex color="white" borderRadius="lg" borderWidth="2px" m="2" p="2">
-      <Box minW="50px" w="5vw" p={2} m={1}>
+      <Box w="2vw" p={2} m={1}>
         {thread.id && <UpOrDownVote thread={thread} />}
       </Box>
-      <Box
-        minW="200px"
-        w="40vw"
-        p={2}
-        m={1}
-      >
+      <Box minW="200px" w="40vw" p={2} m={1}>
         <Link to={`/topic/${thread.id}`}>
           <Box>
             <Text isTruncated>{thread.title}</Text>
           </Box>
-          <Divider size="sm"/>
+          <Divider size="sm" />
           <Text isTruncated mt={2}>
             {thread.text}
           </Text>
