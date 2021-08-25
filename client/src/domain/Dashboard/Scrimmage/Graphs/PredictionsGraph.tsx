ed8d2f6212b55889ffa44d3b136a02a6139c575b
jchase2 <mr.jdchase@gmail.com>
2021-08-25 17:02:19 -0400

    Bug fix

--- client/src/domain/Dashboard/Scrimmage/Graphs/PredictionsGraph.tsx
+++ client/src/domain/Dashboard/Scrimmage/Graphs/PredictionsGraph.tsx
@@ -1,90 +1,93 @@
-import { CanvasJSChart } from 'canvasjs-react-charts';
-import { Flex, Spacer,  } from "@chakra-ui/react"
-import { useEffect, useState } from 'react';
+import { CanvasJSChart } from "canvasjs-react-charts";
+import { Flex, Spacer } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
-const PredictionsGraph = ({scrim}:any) => {
-  const [, setTeamOne] = useState<any>([])
-  const [, setTeamTwo] = useState<any>([])
+const PredictionsGraph = ({ scrim }: any) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [teamOne, setTeamOne] = useState<any>([]);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [teamTwo, setTeamTwo] = useState<any>([]);
   const [teams, setTeams] = useState<any>({});
-
 
   useEffect(() => {
     if (scrim.player1info) {
-
-      const newTeamOne: any[] = []
-      const newTeamTwo: any[] = []
+      const newTeamOne: any[] = [];
+      const newTeamTwo: any[] = [];
       for (let i = 1; i <= 10; i++) {
-        const rankedInfo = scrim[`player${i}ranked`].map(info => {
-          const newInfo = {
-            queueType: info.queueType,
-            losses: info.losses,
-            rank: info.rank,
-            tier: info.tier,
-            wins: info.wins,
-          }
-          return newInfo
-        }).sort((a, b) => (a.queueType === 'RANKED_SOLO_5x5' ? -1 : 1))
+        const rankedInfo = scrim[`player${i}ranked`]
+          .map((info) => {
+            const newInfo = {
+              queueType: info.queueType,
+              losses: info.losses,
+              rank: info.rank,
+              tier: info.tier,
+              wins: info.wins,
+            };
+            return newInfo;
+          })
+          .sort((a, b) => (a.queueType === "RANKED_SOLO_5x5" ? -1 : 1));
         let player = {
           name: scrim[`player${i}`],
           info: scrim[`player${i}info`],
-          ranked: rankedInfo.length ? rankedInfo[0] : undefined
-        }
+          ranked: rankedInfo.length ? rankedInfo[0] : undefined,
+        };
         if (i <= 5) {
-          newTeamOne.push(player)
+          newTeamOne.push(player);
         } else {
-          newTeamTwo.push(player)
+          newTeamTwo.push(player);
         }
       }
       const predict = (newTeamOne, newTeamTwo) => {
-        let array1:any[] = [];
-        let array2:any[] = [];
+        let array1: any[] = [];
+        let array2: any[] = [];
         let teamsInfo = {
           team1BattleWinAvg: 0,
           team1Wins: 0,
-          team1Losses:0,
+          team1Losses: 0,
           team2BattleWinAvg: 0,
           team2Wins: 0,
-          team2Losses:0
-        }
+          team2Losses: 0,
+        };
         newTeamOne.forEach((player) => {
           let ratio = {
-            avg: player.ranked ? player.ranked.wins /
-              (player.ranked.wins + player.ranked.losses) : 0,
+            avg: player.ranked
+              ? player.ranked.wins / (player.ranked.wins + player.ranked.losses)
+              : 0,
             wins: player.ranked ? player.ranked.wins : 0,
             losses: player.ranked ? player.ranked.losses : 0,
-          }
+          };
           array1.push(ratio);
         });
         newTeamTwo.forEach((player) => {
           let ratio = {
-            avg: player.ranked ? player.ranked.wins /
-              (player.ranked.wins + player.ranked.losses) : 0,
+            avg: player.ranked
+              ? player.ranked.wins / (player.ranked.wins + player.ranked.losses)
+              : 0,
             wins: player.ranked ? player.ranked.wins : 0,
             losses: player.ranked ? player.ranked.losses : 0,
-          }
+          };
           array2.push(ratio);
         });
 
         array1.forEach((player) => {
-          teamsInfo.team1Wins+=player.wins;
-          teamsInfo.team1Losses+=player.losses;
-          teamsInfo.team1BattleWinAvg+=player.avg;
+          teamsInfo.team1Wins += player.wins;
+          teamsInfo.team1Losses += player.losses;
+          teamsInfo.team1BattleWinAvg += player.avg;
         });
         array2.forEach((player) => {
-          teamsInfo.team2Wins+=player.wins;
-          teamsInfo.team2Losses+=player.losses;
-          teamsInfo.team2BattleWinAvg+=player.avg;
+          teamsInfo.team2Wins += player.wins;
+          teamsInfo.team2Losses += player.losses;
+          teamsInfo.team2BattleWinAvg += player.avg;
         });
 
         console.log(teamsInfo);
         setTeams(teamsInfo);
-      }
+      };
       predict(newTeamOne, newTeamTwo);
       setTeamOne(newTeamOne);
       setTeamTwo(newTeamTwo);
     }
-  }, [scrim])
-
+  }, [scrim]);
 
   return (
     <Flex>
@@ -171,6 +174,6 @@ const PredictionsGraph = ({scrim}:any) => {
       />
     </Flex>
   );
-}
+};
 
-export default PredictionsGraph
+export default PredictionsGraph;
