567e2d21c2c9c28b556bbe91f2f44ee20707fe25
jchase2 <mr.jdchase@gmail.com>
2021-08-25 18:13:53 -0400

    Changes for AWS db

--- server/src/Models/index.ts
+++ server/src/Models/index.ts
@@ -7,12 +7,13 @@ import { Votes } from './vote.model';
 import { Matches } from './match.model';
 import { Scrimmages } from './scrimmage.model';
 
-const {DATABASE_TEST, DATABASE, NODE_ENV, DB_USERNAME, DB_PW} = process.env;
+const {DATABASE_TEST, DATABASE, NODE_ENV, DB_USERNAME, DB_PW, DB_HOST} = process.env;
 
 export const sequelize = new Sequelize({
   models: [User, Friend, Region, Topic, Votes, Matches, Scrimmages],
   database: NODE_ENV === 'test' ? DATABASE_TEST : DATABASE,
   dialect: 'postgres',
+  host: DB_HOST,
   logging: false,
   username: DB_USERNAME,
   password: DB_PW,
