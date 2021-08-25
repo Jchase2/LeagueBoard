83d26e79811dec8637db2269b2f5e0615cc336b5
jchase2 <mr.jdchase@gmail.com>
2021-08-25 19:25:41 -0400

    Removing unused files, converted favicon

--- client/src/redux/reducers/index.tsx
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import scrimmageReducer from './scrimmageReducer';
-
-
-export default combineReducers({
-  // put all individual reducers here
-  scrimmages: scrimmageReducer, 
-});
