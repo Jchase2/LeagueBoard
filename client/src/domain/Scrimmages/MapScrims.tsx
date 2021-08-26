a30ebc57305a25f3e698bb111d914ee839cc465c
jchase2 <mr.jdchase@gmail.com>
2021-08-25 20:34:26 -0400

    Fixing not-iterable error when pushign back button

--- client/src/domain/Scrimmages/MapScrims.tsx
+++ client/src/domain/Scrimmages/MapScrims.tsx
@@ -4,13 +4,10 @@ const MapScrims: React.FC<{scrims:any}> = ({scrims}) => {
   return (
     <>
       {
-      scrims && [...scrims].sort((a, b) => Number(b.date) - Number(a.date)).map((scrim) => (
-
+      scrims.length && [...scrims].sort((a, b) => Number(b.date) - Number(a.date)).map((scrim) => (
         <div key={scrim.id}>
             {!(scrim.parentid >= 1) && <ScrimCard key={scrim.id} scrim={scrim} />}
         </div>
-
-
       ))
       }
     </>
